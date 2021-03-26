3e50bc35c9ea85732fa5b617c0a0f6ae08f7ae6a
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2021-03-26 23:33:18 +0800

    Migrating signal placeholders to correct boost naming scheme. (#9)

--- src/qt/clr/clrgui.cpp
+++ src/qt/clr/clrgui.cpp
@@ -688,11 +688,11 @@ static bool ThreadSafeMessageBox(ClearCoinGUI* gui, const std::string& message, 
 void ClearCoinGUI::subscribeToCoreSignals()
 {
     // Connect signals to client
-    uiInterface.ThreadSafeMessageBox.connect(boost::bind(ThreadSafeMessageBox, this, _1, _2, _3));
+    uiInterface.ThreadSafeMessageBox.connect(boost::bind(ThreadSafeMessageBox, this, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
 }
 
 void ClearCoinGUI::unsubscribeFromCoreSignals()
 {
     // Disconnect signals from client
-    uiInterface.ThreadSafeMessageBox.disconnect(boost::bind(ThreadSafeMessageBox, this, _1, _2, _3));
+    uiInterface.ThreadSafeMessageBox.disconnect(boost::bind(ThreadSafeMessageBox, this, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3));
 }
