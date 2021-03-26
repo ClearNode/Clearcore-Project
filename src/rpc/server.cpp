f25ed6a9dd7f93ea24cb2b905b22707183a1e0f1
risingstar2018 <33366277+risingstar2018@users.noreply.github.com>
2021-03-26 23:40:49 +0800

    Migrating signal placeholders to correct boost naming scheme. (#10)
    * update codebase
    
    * Update README.md
    
    * [Bugfix] rename
    
    * [Bugfix] update folder name
    
    * Update style_light.css
    
    * Migrating signal placeholders to correct boost naming scheme.

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -64,12 +64,12 @@ void RPCServer::OnStopped(std::function<void ()> slot)
 
 void RPCServer::OnPreCommand(std::function<void (const CRPCCommand&)> slot)
 {
-    g_rpcSignals.PreCommand.connect(boost::bind(slot, _1));
+    g_rpcSignals.PreCommand.connect(boost::bind(slot, boost::placeholders::_1));
 }
 
 void RPCServer::OnPostCommand(std::function<void (const CRPCCommand&)> slot)
 {
-    g_rpcSignals.PostCommand.connect(boost::bind(slot, _1));
+    g_rpcSignals.PostCommand.connect(boost::bind(slot, boost::placeholders::_1));
 }
 
 void RPCTypeCheck(const UniValue& params,
@@ -570,7 +570,7 @@ std::vector<std::string> CRPCTable::listCommands() const
 
     std::transform( mapCommands.begin(), mapCommands.end(),
                    std::back_inserter(commandList),
-                   boost::bind(&commandMap::value_type::first,_1) );
+                   boost::bind(&commandMap::value_type::first,boost::placeholders::_1) );
     return commandList;
 }
 
